6b524c40c64ad5334fa0f8b87da3eb274d01d183
Femi <femisoyombo@gmail.com>
2015-07-02 12:58:21 -0400

    ApplicationBasics class created to retrive values from web.config file

---
+++ ShawProject/DataLayer/ApplicationBasics/ApplicationBasic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.ApplicationBasics
+{
+    public class ApplicationBasic
+    {
+        private ApplicationBasic() { }
+
+        private static string url;
+
+        private static ApplicationBasic _applicationBasic;
+
+        public static ApplicationBasic GetApplicationBasicInstance()
+        {
+            return _applicationBasic ?? (_applicationBasic = new ApplicationBasic());
+        }
+
+        public static string Url
+        {
+            get
+            {
+                url = ConfigurationManager.AppSettings["urlString"];
+                return url;
+            }
+        }
+
+    }
+}
