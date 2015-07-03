f6e15b0ac80ccd5818ab26bdb2b08435ca46d07e
Femi <femisoyombo@gmail.com>
2015-07-03 18:04:01 -0400

    ResultModelSortByName class created to sort list

---
+++ ShawProject/DataLayer/Model/ResultModelSortByName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLayer.Model
+{
+     public class ResultModelSortByName: IComparer<ResultModel>
+     {
+         #region IComparer<ResultModel> Members
+
+         public int Compare(ResultModel obj, ResultModel otherObj)
+         {
+             return String.CompareOrdinal(obj.Name, otherObj.Name);
+         }
+
+         #endregion
+    }
+}
